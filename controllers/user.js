ef3cb5799ea31fadeea2806e16917938d66055be
Matthew Nte <Matthewnte1@gmail.com>
2019-11-12 00:49:18 +0100

    feature(sign in): admin and employees can sign in [Finishes #12]

--- controllers/user.js
+++ controllers/user.js
@@ -8,5 +8,45 @@ exports.creatUser = (req, res) => {
 };
 
 exports.signin = (req, res, next) => {
-
+    const user = {
+        email: req.body.email,
+        password: req.body.password,
+    }
+    pool.query('SELECT email, password FROM users WHERE email = $1', [user.email], (err, results) => {
+        if (err) {
+            throw err
+        } else if (results.rowCount >= 1) {
+            let password = '';
+            const emailArr = results.rows;
+            emailArr.forEach((item) => password = Object.values(item))
+            bcrypt.compare(user.password, password[1]).then(
+                (valid) => {
+                    if (!valid) {
+                        return res.status(401).json({
+                            error: 'Incorrect password',
+                        })
+                    }
+                    jwt.sign({ user }, 'reqbodypassword', (error, token) => {
+                        res.json({
+                            status: 'success',
+                            data: {
+                                token,
+                                userId: pool.query(`SELECT userId FROM users WHERE email = ${user.email}`),
+                            },
+                        })
+                    })
+                },
+            ).catch(
+                (error) => {
+                    res.status(500).json({
+                        error,
+                    });
+                },
+            )
+        } else {
+            res.status(401).json({
+                error: 'User not found',
+            })
+        }
+    })
 };
